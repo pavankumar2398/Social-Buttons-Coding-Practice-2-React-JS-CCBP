a0adcfd28b4eb7d84a594f8b57bed2109776db4e
pavankumar2398 <mpkumar2398@gmail.com>
2023-08-13 10:31:16 +0000

    Social buttons changed

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 const Button = (props) => {
   //  Write your code here.
   const { className, buttonText } = props;
-  return <button className={className}>{buttonText}</button>;
+  return <button className={`button ${className}`}>{buttonText}</button>;
 };
 
 const element = (
   //  Write your code here.
-  <div className="buttons-container">
+  <div className="social-buttons-container">
     <div className="content-container">
-      <h1 className="heading">Social Buttons </h1>
-      <div className="buttons_container-edit">
-        <Button buttonText="Like" className="button like-button" />
-        <Button buttonText="Comment" className="button comment-button" />
-        <Button buttonText="Share" className="button share-button" />
+      <h1 className="heading">Social Buttons</h1>
+      <div className="buttons-container">
+        <Button buttonText="Like" className="like-button" />
+        <Button buttonText="Comment" className="comment-button" />
+        <Button buttonText="Share" className="share-button" />
       </div>
     </div>
   </div>
